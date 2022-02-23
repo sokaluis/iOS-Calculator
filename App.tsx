a061c13d8e5973ed8415be6c39956bb8b68c1fde
Luis Azocar <lazocar@Mac-Air.local>
2022-02-23 13:59:04 -0300

    fixing

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
-import CalculadoraScreens from './src/screens/CalculadoraScreens';
-import {globalStyles} from './src/theme/appTheme';
+import React from "react";
+import { SafeAreaView, StatusBar } from "react-native";
+import CalculadoraScreens from "./src/screens/CalculadoraScreens";
+import { globalStyles } from "./src/theme/appTheme";
 
 const App = () => {
   return (
